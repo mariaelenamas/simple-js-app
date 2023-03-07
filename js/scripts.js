90859f17aee89d123307e9aef6ef7efe32ad5729
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-03-07 14:04:22 +0100

    Fixed the array

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ let pokemonList = [
     {name: "Pikachu", height: "104", types: ["eletric"]},
     {name: "Charmeleon", height: "307", types: ["fire"]},
 ];
+
+for (let i=0; i< pokemonList.length; i++) {
+    if(pokemonList[i].height <110) {
+        document.write(pokemonList[i].name + " is a very tiny Pokémon!");
+    }else if(pokemonList[i].height >110 && pokemonList[i].height <300){
+        document.write(pokemonList[i].name + " is a medium-size Pokémon!");
+    }else{
+    document.write(pokemonList[i].name + " is a huge Pokémon!!");
+    }
+}
