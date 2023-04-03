7152a565e2586bff09220948941825de4a9baeec
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-04-03 23:22:28 +0200

    Added pokemonButton

--- js/scripts.js
+++ js/scripts.js
@@ -16,20 +16,37 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
 
+    // function showDetails (pokemon) {
+    //     console.log(pokemon.name)
+    // }
+
+    function addListItem(pokemon) {
+    let ul = document.querySelector('.pokemon-list'); 
+    let listItem = document.createElement('li'); 
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add("pokemonButton");
+    listItem.appendChild(button);
+    pokemonListItems.appendChild(listItem);
+
+    }
+
+
+    // pokemonRepository.getAll().forEach(function(pokemon) {
+    //     if(pokemon.height <110) {
+    //         document.write(pokemon.name + " is a very tiny Pokémon!<br>");
+    //     }else if(pokemon.height >110 && pokemon.height <300){
+    //         document.write(pokemon.name + " is a medium-size Pokémon!<br>");
+    //     }else{
+    //     document.write(pokemon.name + " is a huge Pokémon!!<br>");
+    //     }
+    // })
+    
+    pokemonList.forEach(addListItem);
+
     return {
         add: add,
         getAll: getAll
     };
 
 })();
-
-    pokemonRepository.getAll().forEach(function(pokemon) {
-        if(pokemon.height <110) {
-            document.write(pokemon.name + " is a very tiny Pokémon!<br>");
-        }else if(pokemon.height >110 && pokemon.height <300){
-            document.write(pokemon.name + " is a medium-size Pokémon!<br>");
-        }else{
-        document.write(pokemon.name + " is a huge Pokémon!!<br>");
-        }
-    })
-    
